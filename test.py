9abca4770f5fb1f5e3ce15cc4cfb99741b89594e
SumitGujrathi <112178227+SumitGujrathi@users.noreply.github.com>
2023-02-11 17:34:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-print("running")
+#print("running")
 
-#cur_time = datetime.now().strftime('%H:%M:%S')
-#cur_time = datetime.strptime(cur_time, '%H:%M:%S').time()
-#print(cur_time)
+cur_time = datetime.now().strftime('%H:%M:%S')
+cur_time = datetime.strptime(cur_time, '%H:%M:%S').time()
+print(cur_time)
 
 
 
