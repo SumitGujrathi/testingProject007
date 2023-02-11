fa0433cba2eef82bb9d449c1e2248e36ff42acde
SumitGujrathi <112178227+SumitGujrathi@users.noreply.github.com>
2023-02-11 09:56:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,67 +42,15 @@ def Pull_Chain_Data(Symbol):
 
     DATA_NSE = db[Collection]
 
+      
+    # Date  format => YYYY,MM,DD, HH,MM,SS
+    Pull_Time = datetime.now()
+    OI = {"Date":Pull_Time, "PE_OI": 1,"CE_OI":1, "PE_COI":1, "CE_COI":1, "NIFTY":1, "PCR":1, "PE_Volatility": 0, "CE_Volatility": 1}
+    DATA_NSE.insert_one(OI)
+    print(OI(
 
-    # NSE live data Option Chain Nifty
-
-    url = f'https://www.nseindia.com/api/option-chain-indices?symbol={Symbol}'  # for NIFTY -BANKNIFTY Copy-paste
-
-
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
-                            'like Gecko) '
-                            'Chrome/80.0.3987.149 Safari/537.36',
-            'accept-language': 'en,gu;q=0.9,hi;q=0.8', 'accept-encoding': 'gzip, deflate, br'}
-    session = requests.Session()
-    request = session.get(url, headers=headers)
-    cookies = dict(request.cookies)
-
-    # print(response.status_code)
-    # DATA = response.content
-
-    # list of item to be data framed => 1 = 'expiryDates' , 2 = 'data' , 3 = 'underlyingValue' , 4 = 'strikePrices'
-
-    try:
-        DATA = session.get(url, headers=headers, timeout=0.5, cookies=cookies).json()['records']['data']
-
-        Cols = ['PE.underlyingValue','expiryDate','PE.openInterest','PE.changeinOpenInterest','PE.impliedVolatility','PE.lastPrice','PE.totalBuyQuantity','PE.totalSellQuantity','strikePrice','CE.totalSellQuantity','CE.totalBuyQuantity','CE.lastPrice','CE.impliedVolatility','CE.changeinOpenInterest','CE.openInterest']
-
-        df = pd.json_normalize(DATA)
-
-        df = df.fillna(0)
-
-        df = df[Cols]
-
-        # Output Data
-
-        PE_OI = df['PE.openInterest'].sum()
-        PE_COI =  df['PE.changeinOpenInterest'].sum()
-        PE_Volat =  np.round(df['PE.impliedVolatility'].mean(),2)
-
-        CE_OI = df['CE.openInterest'].sum()
-        CE_COI = df['CE.changeinOpenInterest'].sum()
-        CE_Volat = np.round(df['CE.impliedVolatility'].mean(),2)
-
-        NIFTY = df['PE.underlyingValue'].max()
-        PCR = np.round(((PE_COI / CE_COI) -1),2)
-
-        # Date  format => YYYY,MM,DD, HH,MM,SS
-        Pull_Time = datetime.now()
-        OI = {"Date":Pull_Time, "PE_OI":PE_OI,"CE_OI":CE_OI, "PE_COI":PE_COI, "CE_COI":CE_COI, "NIFTY":NIFTY, "PCR":PCR, "PE_Volatility": PE_Volat, "CE_Volatility": CE_Volat}
-
-        DATA_NSE.insert_one(OI)
-
-        print(OI)
-
-
-
-        # OI Data & save to CSV file
-
-        # OI_DATA.to_csv(f'{path}\\OPTION_OI_DATA.csv', mode='a', index=False, header=False)
-
-    except:
-        pass
-
-
+ 
+Pull_Chain_Data(Symbol="NIFTY")
 
 # Date Setting 
 
@@ -113,7 +61,7 @@ print(Today_Date)
 # On_Day = Today_Date in OffDates # If False Market is On.
 On_Day = Today_Date in OffDates
 
-print(On_Day)
+# print(On_Day)
 
 
 # Start Time Setting 
@@ -121,115 +69,5 @@ print(On_Day)
 cur_time = datetime.now().strftime('%H:%M:%S')
 cur_time = datetime.strptime(cur_time, '%H:%M:%S').time()
 print(cur_time)
-
-
-# Start Time Setting
-
-Start_time = '00:30:00' # time on web 09:17:00 in india
-Start_time = datetime.strptime(Start_time, '%H:%M:%S').time()
-
-
-# Stop Time Setting 
-
-Stop_time = '01:30:00'   # time on web 15:28:00 in india
-Stop_time = datetime.strptime(Stop_time, '%H:%M:%S').time()
-
-
-restart = True
-# def date_input():
-#     if (Today_Date == input()):
-#         restart = True
-
-while restart:
-
-    if (On_Day == False):
-
-        # Date Setting 
-        Today_Date = datetime.now().strftime('%m-%d-%Y')
-        Today_Date = Today_Date
-        On_Day = Today_Date in OffDates
-        
-        restart = True
-
-        while restart:
-
-            if(cur_time > Start_time and cur_time < Stop_time):
-                restart = True
-                # Current Time Setting 
-                cur_time = datetime.now().strftime('%H:%M:%S')
-                cur_time = datetime.strptime(cur_time, '%H:%M:%S').time()
-                cur_time = cur_time
-
-                # Main Script here below =>
-
-
-                Pull_Chain_Data("NIFTY")
-                Pull_Chain_Data("BANKNIFTY")
-
-
-
-
-                # <= Main Script End =>
-                print(cur_time)
-                print("Pulling")
-                time.sleep(240)
-                
-            else:
-                # Current Time Setting
-                cur_time = datetime.now().strftime('%H:%M:%S')
-                cur_time = datetime.strptime(cur_time, '%H:%M:%S').time()
-                cur_time = cur_time
-
-
-                print("Time Over")
-                print(cur_time)
-                restart = "Wait"      
-
-                Today_Date = datetime.now().strftime('%m-%d-%Y')
-                Today_Date = Today_Date    
-                On_Day = Today_Date in OffDates
-                # On_Day = input() in OffDates
-
-
-                if (On_Day == False):
-                # Date Setting 
-
-                    Today_Date = datetime.now().strftime('%m-%d-%Y')
-                    Today_Date = Today_Date
-                    On_Day = Today_Date in OffDates
-
-                    time.sleep(1)
-                    restart = "Wait"
-                time.sleep(50)
-                break
-
-                # restart = False
-
-    else:
-
-        cur_time = datetime.now().strftime('%H:%M:%S')
-        cur_time = datetime.strptime(cur_time, '%H:%M:%S').time()
-        cur_time = cur_time
-
-
-        print("Off Day")
-        print(cur_time)
-        restart = "Wait"
-        Today_Date = datetime.now().strftime('%m-%d-%Y')
-        Today_Date = Today_Date    
-        On_Day = Today_Date in OffDates
-        # On_Day = input() in OffDates
-      
-        if (On_Day == True):
-        # Date Setting 
-
-            Today_Date = datetime.now().strftime('%m-%d-%Y')
-            Today_Date = Today_Date
-            On_Day = Today_Date in OffDates
-
-            time.sleep(1)
-            restart = True
-        time.sleep(43200)         
-
 
 
